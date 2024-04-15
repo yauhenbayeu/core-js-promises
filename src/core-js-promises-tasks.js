ccda842ad40abaaf1e352087bcba390fa7a496fd
Yauhen Bayeu <bayeu.yauhen@reliableopensource.com>
2024-04-15 23:47:48 +0200

    feat: queuPromises task

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -147,8 +147,20 @@ function getAllResult(promises) {
  * [promise1, promise4, promise3] => Promise.resolved('104030')
  * [promise1, promise4, promise3, promise2] => Promise.resolved('10403020')
  */
-function queuPromises(/* promises */) {
-  throw new Error('Not implemented');
+function queuPromises(promises) {
+  let resultString = '';
+  let queue = Promise.resolve();
+
+  promises.forEach((promise) => {
+    queue = queue
+      .then(() => promise)
+      .then((value) => {
+        resultString += value;
+        return resultString;
+      });
+  });
+
+  return queue;
 }
 
 module.exports = {
